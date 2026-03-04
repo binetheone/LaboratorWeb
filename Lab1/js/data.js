cd6c9c1bd32196f65551460988ea0342cd7e2475
Vlădoiu Eusebio Sorin <vladoiusorin089@gmail.com>
2026-03-04 16:54:10 +0200

    feat: adaugă salut personalizat bazat pe ora zilei

--- Lab1/js/data.js
+++ Lab1/js/data.js
@@ -1,11 +1,11 @@
 const hour = new Date().getHours();
 
-const headerParagraph = document.querySelector("#header");
+const headerParagraph = document.querySelector("header p");
 
 if (hour >= 4 && hour <= 11) {
     headerParagraph.textContent = "Bună dimineața! Bine ai venit pe pagina mea.";
-} else if (hour >= 12 && hour <= 17) {
-    headerParagraph.textContent = "Bună ziua! Bine ai venit pe pagina mea.";
+} else if (hour >= 12 && hour <= 18) {
+    headerParagraph.textContent = "Bună ziua!. Bine ai venit pe pagina mea.";
 } else {
     headerParagraph.textContent = "Bună seara! Bine ai venit pe pagina mea.";
 }
