7beebf2d30b16df3171b54c4e08a2730eff5b145
Vlădoiu Eusebio Sorin <vladoiusorin089@gmail.com>
2026-03-11 17:00:54 +0200

    feat: sa facaut ex3 de la lab 1

---
+++ Lab1/js/contact-form.js
@@ -0,0 +1,12 @@
+function submitForm() {
+
+  const nume = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mesaj = document.getElementById("message").value;
+
+  console.log(nume);
+  console.log(email);
+  console.log(mesaj);
+
+  console.warn("Goodbye World!");
+}
