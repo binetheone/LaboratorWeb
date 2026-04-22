d004c2b7ff39448a9d5d70f75e26932cc970ca13
Vlădoiu Eusebio Sorin <vladoiusorin089@gmail.com>
2026-04-22 16:39:23 +0300

    Instalare și setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import Navbar from "./Navbar";
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 function App() {
  return (
  <BrowserRouter>
+ <Navbar />
  <Routes>
  <Route path="/" element={<Home />} />
  <Route path="/projects" element={<Projects />} />
